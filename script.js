4d4d9e45266ee436fbf76f1977ebf6ef32ebbc94
hammad hassan <hammadhassan4589@gmil.com>
2023-09-08 07:04:03 +0500

    java code

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstname = "hammad";
+
+var lastname ="hassan";
+
+var biscuit = "bisca";
+
+var answer = "hyper text markup language" ;
+
+ var question = prompt("what is the full form of HTML?")
+
+alert( "congartulations" + " " + firstname + " "+  "have won the" + " " + biscuit + "!" )
