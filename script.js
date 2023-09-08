beaf7248cc6cf5a9633d183b9283254c2f0e1869
hammad hassan <hammadhassan4589@gmil.com>
2023-09-08 21:21:40 +0500

    conditions

--- script.js
+++ script.js
@@ -1,11 +1,76 @@
-var firstname = "hammad";
+// letters answer practice
 
-var lastname ="hassan";
 
-var biscuit = "bisca";
+// var firstname = "hammad";
 
-var answer = "hyper text markup language" ;
+// var lastname ="hassan";
 
- var question = prompt("what is the full form of HTML?")
+// var biscuit = "bisca";
+// var answer;
+// answer = "hyper" ;
+// answer = true;
 
-alert( "congartulations" + " " + firstname + " "+  "have won the" + " " + biscuit + "!" )
+//  var question = prompt("what H stands for in HTML abbr?");
+// if (true) 
+//     alert( "congartulations" + " " + firstname + " "+  "have won the" + " " + biscuit + "!" );
+
+//     else if (false  )
+//     alert("please try again!");
+
+
+
+
+
+//  digital answer practice
+
+// let hour = 12;
+//  let question;
+//  question = prompt("what time is it? ")
+
+// if (hour == 12 && hour < 12)
+//     alert("good morning")
+
+// else if(hour > 12 && hour < 18) 
+//     alert("good afternoon");
+
+// else  (hour > 18 )
+//     alert("good night");
+
+
+// switch and case 
+
+var biscuit = prompt("which biscuit do you want ? ");
+
+switch (biscuit){
+    case "BISCA":
+        alert("bisca 20 rupees ka ha ");
+        break;
+        case "PEANUT":
+            alert("peanut 40 rupees ka ha");
+            break;
+            case "SUPER":
+                alert("SUPER 30 rupees ka ha");
+                break;
+                case "PRINCE":
+                    alert("PRINCE 30 rupees ka ha");
+                    break;
+                    case "BUTTER CRUNCH":
+                        alert("BUTTER CRUNCH 30 rupees ka ha");
+                        break;
+                        case "RIO":
+                            alert("RIO 40 rupees ka ha");
+                            break;
+                            case "OREO":
+                                alert("OREO 30 rupees ka ha");
+                                break;
+                                case "CHOCOLATO":
+                                    alert("CHOCOLATO 30 rupees ka ha");
+                                    break;
+                                    case "LEMON SANDWICH":
+                                        alert("LEMON SANDWICH 40 rupees ka ha");
+                                        break;
+                                        default:
+                                            alert (`${biscuit} "is not availabe ` ) ;
+                                           
+            
+}
