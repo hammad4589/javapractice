fa859e2db73de95bde699b939a845bd750fdc9ba
hammad hassan <hammadhassan4589@gmil.com>
2023-10-15 10:12:17 +0500

    practice

--- script.js
+++ script.js
@@ -39,38 +39,152 @@
 
 // switch and case 
 
-var biscuit = prompt("which biscuit do you want ? ");
+// var biscuit = prompt("which biscuit do you want ? ");
 
-switch (biscuit){
-    case "BISCA":
-        alert("bisca 20 rupees ka ha ");
-        break;
-        case "PEANUT":
-            alert("peanut 40 rupees ka ha");
-            break;
-            case "SUPER":
-                alert("SUPER 30 rupees ka ha");
-                break;
-                case "PRINCE":
-                    alert("PRINCE 30 rupees ka ha");
-                    break;
-                    case "BUTTER CRUNCH":
-                        alert("BUTTER CRUNCH 30 rupees ka ha");
-                        break;
-                        case "RIO":
-                            alert("RIO 40 rupees ka ha");
-                            break;
-                            case "OREO":
-                                alert("OREO 30 rupees ka ha");
-                                break;
-                                case "CHOCOLATO":
-                                    alert("CHOCOLATO 30 rupees ka ha");
-                                    break;
-                                    case "LEMON SANDWICH":
-                                        alert("LEMON SANDWICH 40 rupees ka ha");
-                                        break;
-                                        default:
-                                            alert (`${biscuit} "is not availabe ` ) ;
+// switch (biscuit){
+//     case "BISCA":
+//         alert("bisca 20 rupees ka ha ");
+//         break;
+//         case "PEANUT":
+//             alert("peanut 40 rupees ka ha");
+//             break;
+//             case "SUPER":
+//                 alert("SUPER 30 rupees ka ha");
+//                 break;
+//                 case "PRINCE":
+//                     alert("PRINCE 30 rupees ka ha");
+//                     break;
+//                     case "BUTTER CRUNCH":
+//                         alert("BUTTER CRUNCH 30 rupees ka ha");
+//                         break;
+//                         case "RIO":
+//                             alert("RIO 40 rupees ka ha");
+//                             break;
+//                             case "OREO":
+//                                 alert("OREO 30 rupees ka ha");
+//                                 break;
+//                                 case "CHOCOLATO":
+//                                     alert("CHOCOLATO 30 rupees ka ha");
+//                                     break;
+//                                     case "LEMON SANDWICH":
+//                                         alert("LEMON SANDWICH 40 rupees ka ha");
+//                                         break;
+//                                         default:
+//                                             alert (`${biscuit} "is not availabe ` ) ;
                                            
             
-}
+// }
+  
+
+
+// last = "hassan";
+
+// first = "hammad";
+// var fullName = first + last ;
+
+
+// alert(fullName);
+
+
+// var numb = "how'ld you like to eat ";
+
+// console.log(numb);
+
+// string to number
+
+// var nume = "12345";
+
+// console.log(Number(nume));
+
+// //  ans 12345
+
+
+// // number to string
+
+// var str = 123;
+
+// console.log(str.toString());
+
+// // temlate literals 
+
+// console.log(`${nume + str}   this is good ${str}`);
+
+
+// //ans  12345123 this is a good 123
+
+// // 
+
+
+// //  ho to find length index ets of strings
+
+
+// var strin = " how old are you?"
+
+// console.log(strin.length);
+
+// // ans 15
+
+// console.log(strin[1])
+// // ans 0
+// console.log(strin[5]);
+// // ans d
+
+
+// // slice 
+
+// console.log(strin.slice(0,9));
+
+// // ans   how old 
+
+
+// // to upper case 
+
+
+// var stru = "my name is hammad hassan";
+
+// console.log(stru.toUpperCase());
+
+// // ANS MY NAEM IS HAMMAD HASSAN 
+
+
+
+
+// // functions 
+
+// function cookTea(steelBowl, ingredients ,time  ){
+
+// console.log( "the steel bowl first be filled with water and placed on the stove" +" "
+//                 + ingredients + " "+ "cups of water with sugar and patti"+ " "
+//                 + time + ""+  "minutes will be required for making it")
+//             }
+
+//             cookTea(3 , 4 ,7);
+
+            //Ans  the steel bowl first be filled with water and placed on the stove 4 cups of water with sugar and patti 7minutes will be required for making it.
+
+
+
+
+
+            //  function with return value 
+
+
+            // let chocolate = prompt("which chocolate do you like most?");
+            // let product = prompt("who makes this chocolate ; means comapny name?")
+            // let price = prompt("what is the actual price of it? ");
+
+            // function asking(chocolate1 , product1, price1){
+            //     let answer = chocolate1 + " " + "chocolate" +" " + "is "    + "made by" + " " + product1 + " " +"and it cost " + " "
+            //     + price1  +" " +"rupees";
+            //     return answer;
+            // }
+
+            // let final = asking(chocolate,product,price);
+
+            // console.log(final);
+
+
+            // Ans dairy milk chocolate is made by cadbury and it cost  50 rupees
+
+
+
